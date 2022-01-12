4903679c81a068ac5260429ddd8c5ba5a8a5f5a3
centauri1852 <felipevelr182@gmail.com>
2022-01-12 13:00:03 -0500

    creacion de html y js archivo con formulas

--- figuras.js
+++ figuras.js
@@ -58,19 +58,19 @@ console.groupEnd();*/
 //cuando construimos la funcion, los valores son llamados parametros.
 //cuando llamamos la funcion, los datos son llamados argumentos.
 function perimetroTriangulo(lado1, lado2, base){
-return lado1 + lado2 + base;
+return lado1 + lado2 + base +"cm";
 
 }
 
 function areaTriangulo(base, altura){
-return (base * altura)/2;
+return (base * altura)/2 + "cm^2";
 }
 
 
 
 // Código del círculo
 
-console.group("Círculos");
+/*console.group("Círculos");
 
 // Radio
 const radioCirculo = 4;
@@ -79,10 +79,10 @@ console.log("El radio del círculo es:" + radioCirculo + "cm");
 //Diámetro
 const diametroCirculo = radioCirculo * 2;
 console.log("El diámetro del círculo es:" + diametroCirculo + "cm");
-
+*/
 //PI
 const PI = Math.PI;
-console.log("PI es" + PI );
+/*console.log("PI es" + PI );
 
 //Círcunferencia
 const perimetroCirculo = diametroCirculo * PI;
@@ -93,8 +93,189 @@ const areaCirculo = PI * (radioCirculo * radioCirculo);
 console.log("El área del círculo es:" + areaCirculo + "cm^2");
 
 console.groupEnd();
-
+*/
 //fin del ejercicio
     
 
+
+//Creando funcion para 
+//En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+//La función debe recibir, como parámetros, la longitud de los 3 lados del triángulo.
+//La función debe validar que la longitud de los 3 lados del triángulo corresponden a un triángulo isósceles.
+//La función debe retornar la altura del triángulo.
+//Pista: la función Math.sqrt de JavaScript puede ayudarte a calcular raíces cuadradas.
+
+
+//La forma de determinar la altura para el ejercicio sugerido es 
+//h = Raiz Cuadrada de (a*a) - ((b*b)/4)  
+//Math.sqrt(numero) = genera el valor de raiz cuadrada
+
+
+//valores del triangulo isoceles
+/*
+cateto a 
+cateto b
+hipotenusa h
+
+*/
+
+/*const catetoa = ladoA;
+
+const catetoa2 = catetoa * catetoa;
+
+const catetob = ladoB;
+
+const catetob2 = (catetob * catetob)/4;
+*/
+
+function catetoa2(a){
+    return a*a;
+}
+
+function catetob2(b){
+    return (b * b)/4;
+}
+
+function hypotenusa (a, b){
+    const catetoaa2 = catetoa2(a);
+    const catetobb2 = catetob2(b);
+    
+    const hypotenusasinraiz = catetoaa2 - catetobb2;
+
+    const h = Math.sqrt(hypotenusasinraiz);
+
+    alert(h);
+
+}
+/*const hipotenusa = Math.sqrt(valor);*/
+
+
+
+
+
 //funciones del círculo
+
+function diametroCirculo(radio){
+    return radio * 2;
+}
+
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio)
+    return diametro * PI + "cm";
+
+}
+
+function areaCirculo(radio){
+    return PI * (radio * radio) + "cm^2";
+}
+
+
+//funciones para pagina web
+//aquí interactuamos con el HTML cuadrado
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+
+    alert(perimetro);
+
+}
+
+function calcularAreaCuadrado() {
+
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+
+    alert(area);
+
+}
+
+//aquí interactuamos con el HTML triángulo
+
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("lado1");
+    const value1 = input1.value;
+
+    const input2 = document.getElementById("lado2");
+    const value2 = input2.value;
+
+    const base = document.getElementById("base");
+    const value3 = base.value;
+
+    const perimetro = perimetroTriangulo(Number(value1), Number(value2), Number(value3));
+
+    alert(perimetro);
+
+}
+
+function calcularAreaTriangulo() {
+
+    const input1 = document.getElementById("lado1");
+    const value1 = input1.value;
+
+    const base = document.getElementById("base");
+    const value3 = base.value;
+
+    const area = areaTriangulo(Number(value1), Number(value3));
+
+    alert(area);
+
+}
+
+
+
+
+//aquí interactuamos con el HTML círculo
+
+
+function calcularPerimetroCirculo() {
+    const input = document.getElementById("inputCirculo");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo(value);
+
+    alert(perimetro);
+
+}
+
+function calcularAreaCirculo() {
+
+    const input = document.getElementById("inputCirculo");
+    const value = input.value;
+
+    const area = areaCirculo(value);
+
+    alert(area);
+
+}
+
+
+//aqui interactuamos con el HTML triángulo Isóceles
+
+
+function calcularAlturaIsoceles() {
+    
+    const inputa = document.getElementById("inputCatetoA");
+    const valuea = inputa.value;
+
+    const inputb = document.getElementById("inputCatetoB");
+    const valueb = inputb.value;
+
+    const area = hypotenusa(Number(valuea), Number(valueb));
+
+    alert(area);
+
+}
+
+
+
+
+
+
+
+
