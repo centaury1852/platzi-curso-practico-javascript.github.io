f241dce8872bfca9f7a49aede613cab86bad081a
centauri1852 <felipevelr182@gmail.com>
2022-01-16 17:09:02 -0500

    actualizando funciones para hacerlas inteligentes

--- descuentos.js
+++ descuentos.js
@@ -24,4 +24,22 @@ function calcularPrecioConDescuento (precio, descuento) {
     return precioConDescuento;
 };
 
+function onClickButtonPriceDiscount (){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;    
+    /*const resultP = document.getElementById("Result");
+    console.log(resultP);
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+
+    */
+}
 
