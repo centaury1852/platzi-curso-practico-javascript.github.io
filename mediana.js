29fc04a2a20af1bcded60cdfce16b847fb386010
centauri1852 <felipevelr182@gmail.com>
2022-01-19 18:04:51 -0500

    actualizando metodologia de como se realizaron los ejercicios

--- mediana.js
+++ mediana.js
@@ -1,3 +1,5 @@
+//creamos un array que contenga una lista de ejemplo
+
 const lista1 = [
     400,
     200,
@@ -7,24 +9,30 @@ const lista1 = [
     40000000,
 ];
 
+// dividmos la cantidad de elementos por 2 para saber la mitad del array,
+//luego aproximamos con parseInt para que sea un numero entero.
+//Por ultimo creamos la variable mitadListaUno para almacenar este numero entero.
 const mitadListaUno = parseInt(lista1.length/ 2);
 
+//teniendo el array, utilizo el metodo sort() que me permite ordenar. a - b, ordena de menor a mayor, b - a de mayor a menor.
 
 lista1.sort(function(a, b){
     return a -  b;
 });
 
+// se crea la funcion para saber si el numero total de elementos es un numero par o impar. Se divide por 2 y si el modulo resultante
+//es 0, entonces es par y retornara true, en caso contrario, retornara false.
+
 function esPar(numeroprueba){
     if (numeroprueba % 2 === 0){
         return true;
     } else {
-        
         return false;
     }
 };
 
 
-
+//llamo a la funcion CalcularPromedio de la version anterior para utilizarla nuevamente.
 
 function calcularPromedio(lista){
     const sumaLista = lista.reduce(
@@ -37,8 +45,21 @@ function calcularPromedio(lista){
             return promedioLista;
 };
 
+//declaro mediana antes de utilizarla en el siguiente bloque de codigo.
+
 
 let mediana = lista1[mitadListaUno];
+
+
+//se crea un condicional donde si la cantidad de numeros de Lista1 es par, luego con la variable mitadListaUno se halla cual es el numero de la mitad del array.
+//si es un array con numero par se generan dos elementos, elemento 1 y elemento 2, luego elemento1 es asignado que de la lista1 tome la posicion de la mitad -1, 
+//es decir, tome la mitad de arriba y a elemento2 tome la mitad de abajo.
+//luego se llama la funcion calcularPromedio, dentro de un array ([]), se ponen los dos elementos, y esta funcion con estos argumentos se almacena
+//en la variable promedioElemento1y2.
+//luego a la variable mediana se le asigna el resultado que guarde promedioElemento1y2.
+
+//si el numero total de los elementos del array no es par, entonces la mediana es asignada con el numero de Lista1 en la posicion mitadListaUno, es decir, la mitad
+//del array.
 
 
 if (esPar(lista1.length)) {
@@ -54,4 +75,9 @@ if (esPar(lista1.length)) {
 }
     
 
+
+//que neceista la mediana.
+//valores organizados
+//dos valores del centro si es par
+//sumar esos valores y dividirlos por 2.
 
--- promedio.js
+++ promedio.js
@@ -1,17 +1,8 @@
 //calcular el promedio
 //es igual a la suma total de los elementos dividido entre sus elementos
 
-const a = 1;
-const b = 2;
-const c = 3;
 
-
-function sumarElementos (a, b, c){
-    let sumaElementosfinal = a + b +c;
-    return sumaElementosfinal;
-}
-
-
+//se crea un array con numeros como ejemplo.
 const lista1 = [
     1,
     2,
@@ -21,35 +12,44 @@ const lista1 = [
     6,
 ];
 
+
+//declarar variable sumarLista1 en 0, para empezar a almacenar la suma de los elementos del array.
+
 let sumaLista1 = 0;
+
+
+//se hace un ciclo y se itera desde i = 0 //seguir la logica del ciclo// lo cual debe ser almacenado en la variable antes inicializada,
+//sumarLista1 entonces almacenara el valor acumulado (que es ella misma) mas el valor nuevo llamado lista1[i], para efectos de ejemplo
+//si  cuando i =0 en lista[i] = lista[0] es igual a 1, lista[1] = 2 y asi sucesivamente.
+//cada vez que se genere el ciclo, este se guardara en la variable sumarLista1. y aqui se le sumara el valor acumulado más el ultimo valor agregado,
+//asi hasta sumar todos los elementos o como tambien se podria decir, hasta que la condicion del ciclo no se cumpla; sea false. 
 
 for (let i = 0; i <lista1.length; i++) {
     sumaLista1 = sumaLista1 + lista1[i];
 }
 
+
+//luego declaramos una nueva variable que almacenara el resultado entre la variable anterior sumarLista1 / lista1.length, que es igual a dividir la
+//suma de todos los elementos entre el total de elementos del array lista1.
 const promedioLista1 = sumaLista1 / lista1.length;
 
+
+//ahora con base al establecimiento de la funcion de ejemplo se desarrolla una funcion nueva que reciba cualquier valor nuevo.
 // crear una funcion con base en lo anterior
 
 function calcularPromedio(lista){
 
-    /*let sumaLista = 0;
-
-    for (let i = 0; i <lista.length; i++) {
-    sumaLista = sumaLista + lista[i];
-}*/
-
-//alternativa a ciclo for --> metodos de arrays
     const sumaLista = lista.reduce(
         function (valorAcumulado = 0, nuevoElemento) {
             return valorAcumulado + nuevoElemento;
         }
-
     )
 
-const promedioLista = sumaLista / lista.length;
+        const promedioLista = sumaLista / lista.length;
     return promedioLista;
 };
+
+
 
 
 
