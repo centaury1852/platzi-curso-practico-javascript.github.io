c8572a5993279c87255db2454a8cc323d00542e7
centauri1852 <felipevelr182@gmail.com>
2022-01-20 18:56:39 -0500

    empiezo con analisis y salarios.js

--- analisis.js
+++ analisis.js
@@ -1,1 +1,12 @@
 //analisis de salarios
+
+
+//crear variable salariosCol para almacenar los salarios de Colombia, los cuales
+//estan en el otro archivo.
+
+
+const salariosCol = colombia.map()
+
+//creamos la variable y almacenamos el metodo .map(); para extraer información del array de salarios.js
+// .map nos sirve para iterar por cada elemento de colombia.
+//dentro de .map(agregamos una funcion anonima que me permita )
